dddc45b56d7e66bc030c43932aef8dc2c06de236
draganradu <draganaurelradu@gmail.com>
2023-08-20 21:37:23 +0200

    ECVGI-19

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import i18n from './i18n'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 const app = createApp(App)
 
+app.use(store)
 app.use(router)
 app.use(i18n)
 
