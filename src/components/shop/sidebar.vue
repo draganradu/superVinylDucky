fe9098351010a4ffaf587783d15a7de5ab55a903
draganradu <draganaurelradu@gmail.com>
2023-08-21 14:20:38 +0200

    ECBGI-26 Added icons

--- src/components/shop/sidebar.vue
+++ src/components/shop/sidebar.vue
@@ -1,3 +1,41 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let collapsed = ref(false);
+
+
+
+</script>
+
 <template>
-    <div class="col-2">Sidebar</div>
+  <div :class="['col-auto', 'sidebar', collapsed ? 'sidebar-collapse' : '']">
+    <div class="sidebar-item" @click="() => { collapsed = !collapsed }">
+      <i :class="[!collapsed ? 'bi-arrow-bar-left' : 'bi-arrow-bar-right', 'me-2']" />
+    </div>
+    <hr />
+    <div class="sidebar-item">
+      <i class="bi-alarm me-2" />
+      <span>Sidebar</span>
+    </div>
+    <div class="sidebar-item">
+      <i class="bi-alarm me-2" />
+      <span>Sidebar</span>
+    </div>
+    <div class="sidebar-item">
+      <i class="bi-alarm me-2" />
+      <span>Sidebar</span>
+    </div>
+    <hr />
+    <div class="sidebar-item">
+      <i class="bi-person-badge-fill me-2" />
+      <span>Login</span>
+    </div>
+
+  </div>
 </template>
+
+<style >
+.sidebar-collapse span {
+  display: none;
+}
+</style>
