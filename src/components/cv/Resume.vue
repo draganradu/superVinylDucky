55d5c83d6d0a93c4180d59ccea0395c6b4f79c93
draganradu <draganaurelradu@gmail.com>
2023-12-29 12:44:07 +0100

    sprint-8

--- src/components/cv/Resume.vue
+++ src/components/cv/Resume.vue
@@ -16,15 +16,67 @@ const is = store.getters['cv/is'](skill)
 
 </script>
 
-
 <template>
   <Card id="radu-work" class="mt-3">
-    Resume [ECBGI-82]
+    <div class="row">
+      <div class="col-12">
+        Skills [ECBGI-81]
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 mb-3">
+        <h2>Resume</h2>
+        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+          consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in
+          iste officiis commodi quidem hic quas.</p>
+      </div>
+      <div class="mb-3">
+        <hr />
+      </div>
+      
+      <div class="col-6">
+        <h4 class="mb-5">Education</h4>
+
+        <div class="resume-box">
+          <h5>MBA - Bussines Managent </h5>
+          <p>2015-2034</p>
+          <p>Bucharest</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo
+            nibh ante facilisis bibendum.</p>    
+        </div>
+        <div class="resume-box mt-5">
+          <h5>MA - Design and Photography </h5>
+          <p>2015-2034</p>
+          <p>Bucharest</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo
+            nibh ante facilisis bibendum.</p>    
+        </div>
+      </div>
+      <div class="col-6">
+        <h4 class="mb-5">Professional Experience</h4>
+        <div class="resume-box mt-5">
+          <h5>Oneara Health [Scrum master]</h5>
+          <p>2015-2034</p>
+          <p>Bucharest</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo
+            nibh ante facilisis bibendum.</p>    
+        </div>
+        <div class="resume-box mt-5">
+          <h5>Meditools [Full Stack]</h5>
+          <p>2015-2034</p>
+          <p>Bucharest</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo
+            nibh ante facilisis bibendum.</p>    
+        </div>
+        <div class="resume-box mt-5">
+          <h5>Verifon now 2Checkout [front]</h5>
+          <p>2015-2034</p>
+          <p>Bucharest</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo
+            nibh ante facilisis bibendum.</p>    
+        </div>
+        <button type="button" class="btn btn-outline-secondary">More</button>
+      </div>
+    </div>
   </Card>
 </template>
-
-<style lang="scss">
-#radu-work {
-  z-index: 10;
-}
-</style>
